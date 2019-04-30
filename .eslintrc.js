43edebf40990a2d25d7a0c57fac328a5ba81fefb
Origami404 <Origami404@aol.com>
2019-04-30 21:54:45 +0800

    add config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,21 +7,25 @@ module.exports = {
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ['html'],
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    "no-irregular-whitespace": ["error", {
-      "skipComments": true,
-      "skipTemplates": true
-    }],
-    "no-unused-vars": ["warn"]
+    'no-irregular-whitespace': [
+      'error',
+      {
+        skipComments: true,
+        skipTemplates: true
+      }
+    ],
+    'no-unused-vars': ['warn'],
+    'no-multiple-empty-lines': ['off', { max: 5 }],
+    'space-before-function-paren': ['off']
+    //'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
   }
 }
